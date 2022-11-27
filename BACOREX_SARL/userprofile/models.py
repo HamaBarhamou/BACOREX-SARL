bfa84ff026c4a8d94ea7cbb7d3a990e73db67f2c
Hamabarhamou <hama.peinture.barhamou@gmail.com>
2022-11-27 15:12:13 +0100

    modification de user

--- BACOREX_SARL/userprofile/models.py
+++ BACOREX_SARL/userprofile/models.py
@@ -3,7 +3,22 @@ from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
 class User(AbstractUser):
-    ASSISTANT_DAO = 'ASSISTANT_DAO'
+    USER_TYPE_CHOICES = (
+      (1, 'Assistant DAO'),
+      (2, 'Chef Service Etude'),
+      (3, 'Chef Departement Etude'),
+      (4, 'Directeur Generale'),
+      (5, 'admin'),
+      (6, 'Coordinateur des Operations'),
+      (7, 'Conducteurs des Travaux'),
+      (8, 'Chef de Projet'),
+      (9, 'DEGP')
+    )
+
+    fonction = models.PositiveSmallIntegerField(choices=USER_TYPE_CHOICES)
+    avatar = models.ImageField(verbose_name='photo de profile')
+
+    """ ASSISTANT_DAO = 'ASSISTANT_DAO'
     CHEF_SERVICE_ETUDE = 'CHEF_SERVICE_ETUDE'
     CHEF_DEPARTEMENT_ETUDE = 'CHEF_DEPARTEMENT_ETUDE'
     DG = 'DG'
@@ -16,4 +31,4 @@ class User(AbstractUser):
     )
 
     avatar = models.ImageField(verbose_name='photo de profile')
-    fonction = models.CharField(max_length=30, choices=ROLE_CHOICES, verbose_name = 'Fonction')
+    fonction = models.CharField(max_length=30, choices=ROLE_CHOICES, verbose_name = 'Fonction') """
