2f2afd4b41074c893ae10c0ab05450d57cfc63c2
Hamabarhamou <hama.peinture.barhamou@gmail.com>
2022-11-29 22:42:52 +0100

    courier entrant html

--- BACOREX_SARL/gestioncouriers/urls.py
+++ BACOREX_SARL/gestioncouriers/urls.py
@@ -5,5 +5,6 @@ from . import views
 
 urlpatterns = [
     path('message',views.messagerie, name="messagerie"),
-    path('', views.boitemessagerie, name="boitereception")
+    path('', views.boitemessagerie, name="boitereception"),
+    path('entrant', views.courier_entrant, name="entrant")
 ]
--- BACOREX_SARL/gestioncouriers/views.py
+++ BACOREX_SARL/gestioncouriers/views.py
@@ -5,8 +5,12 @@ from django.template import loader
 from userprofile.forms import LoginForm
 from .models import Message
 import datetime
+from django.contrib.auth.decorators import login_required
+
+
 
 # Create your views here.
+@login_required(login_url='/user/')
 def messagerie(request):
     if request.method == 'POST':
         form = MessageForm(request.POST)
@@ -29,8 +33,15 @@ def messagerie(request):
     template = loader.get_template('message.html')
     return HttpResponse(template.render(context, request))
 
-
+@login_required(login_url='/user/')
 def boitemessagerie(request):
     context = {}
     template = loader.get_template('messagerie.html')
+    return HttpResponse(template.render(context, request))
+
+@login_required(login_url='/user/')
+def courier_entrant(request):
+    q = Message.objects.all()
+    context = {'message': q}
+    template = loader.get_template('courier_entrant.html')
     return HttpResponse(template.render(context, request))
